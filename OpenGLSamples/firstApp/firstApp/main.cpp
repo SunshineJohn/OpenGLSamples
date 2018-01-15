2766ceaedf83ba10d30d9b42695de05e14281f97
Vladyslav Selishchev <vselishchev@amcbridge.com>
2018-01-15 08:52:03 +0200

    Tessellated triangle is shown.

--- OpenGLSamples/firstApp/firstApp/main.cpp
+++ OpenGLSamples/firstApp/firstApp/main.cpp
@@ -7,12 +7,6 @@ static const GLchar* vertex_shader_source = GLSL
 (
   450 core,
   layout (location = 0) in vec4 offset;
-  layout(location = 1) in vec4 color;
-
-  out VS_OUT
-  {
-    vec4 color;
-  } vs_out;
 
   void main()
   {
@@ -21,25 +15,55 @@ static const GLchar* vertex_shader_source = GLSL
                                      vec4(0.25, 0.25, 0.5, 1.0));
 
     gl_Position = vertices[gl_VertexID] + offset;
-
-    vs_out.color = color;
   }
 );
 
 static const GLchar* fragment_shader_source = GLSL
 (
   450 core,
-  in VS_OUT
-  {
-    vec4 color;
-  } fs_in;
 
   out vec4 color;
   void main()
   {
-    color = fs_in.color;
+    color = vec4(0.0f, 0.8f, 1.0f, 1.0f);
   }
 );
+
+static const GLchar* tess_control_source = GLSL
+(
+  450 core,
+  layout(vertices = 3) out;
+
+  void main(void)
+  {
+    if (gl_InvocationID == 0)
+    {
+      gl_TessLevelInner[0] = 5.0;
+      gl_TessLevelOuter[0] = 5.0;
+      gl_TessLevelOuter[1] = 5.0;
+      gl_TessLevelOuter[2] = 5.0;
+    }
+
+    gl_out[gl_InvocationID].gl_Position =
+      gl_in[gl_InvocationID].gl_Position;
+  }
+
+);
+
+static const GLchar* tess_evaluation_source = GLSL
+(
+  450 core,
+  layout (triangles, equal_spacing, cw) in;
+
+  void main(void)
+  {
+    gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position +
+                   gl_TessCoord.y * gl_in[1].gl_Position +
+                   gl_TessCoord.z * gl_in[2].gl_Position);
+  }
+);
+
+
 
 class my_application : public sb7::application
 {
@@ -47,15 +71,16 @@ public:
   void startup() override
   {
     rendering_program = compile_shaders();
-    glCreateVertexArrays(1, &vertex_array_object);
+    glGenVertexArrays(1, &vertex_array_object);
     glBindVertexArray(vertex_array_object);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   }
 
   void shutdown() override
   {
     glDeleteVertexArrays(1, &vertex_array_object);
     glDeleteProgram(rendering_program);
-    glDeleteVertexArrays(1, &vertex_array_object);
   }
 
 	void render(double current_time) override
@@ -72,20 +97,16 @@ public:
                           0.0f, 0.0f };
 
     glVertexAttrib4fv(0, attrib);
-
-    GLfloat in_color[] = { (float)sin(current_time)*0.5f + 0.5f,
-                           (float)cos(current_time)*0.5f + 0.5f,
-                            0.0f, 1.0f };
-
-    glVertexAttrib4fv(1, in_color);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    glDrawArrays(GL_PATCHES, 0, 3);
 	}
 
   GLuint compile_shaders(void)
   {
     GLuint vertex_shader;
     GLuint fragment_shader;
+    GLuint control_shader;
+    GLuint evaluation_shader;
     GLuint program;
 
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -96,12 +117,24 @@ public:
     glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
     glCompileShader(fragment_shader);
 
+    control_shader = glCreateShader(GL_TESS_CONTROL_SHADER);
+    glShaderSource(control_shader, 1, &tess_control_source, nullptr);
+    glCompileShader(control_shader);
+
+    evaluation_shader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+    glShaderSource(evaluation_shader, 1, &tess_evaluation_source, nullptr);
+    glCompileShader(evaluation_shader);
+
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
+    glAttachShader(program, control_shader);
+    glAttachShader(program, evaluation_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
 
     glDeleteShader(vertex_shader);
+    glDeleteShader(control_shader);
+    glDeleteShader(evaluation_shader);
     glDeleteShader(fragment_shader);
 
     return program;
