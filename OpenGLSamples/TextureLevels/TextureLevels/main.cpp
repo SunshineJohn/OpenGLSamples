81efbc1de52195e2d1e6865972a3714dc709f689
Vladyslav Selishchev <vselishchev@amcbridge.com>
2018-02-27 17:47:08 +0200

    Added Tunnel project

--- OpenGLSamples/TextureLevels/TextureLevels/main.cpp
+++ OpenGLSamples/TextureLevels/TextureLevels/main.cpp
@@ -112,8 +112,103 @@ void TextureLevels::startup()
     unsigned int r = (random_uint() & 0xFCFF3F) << (random_uint() % 12);
 
     glGenTextures(1, &textures[i].name);
-    glBindTexture(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, textures[i].name);
+    glTexStorage2D(GL_TEXTURE_2D, TEXTURE_LEVELS, GL_RGBA8, 
+                   TEXTURE_SIZE, TEXTURE_SIZE);
+
+    for (j = 0; j < 32 * 32; ++j)
+    {
+      mutated_data[j] = (((unsigned int*)tex_data)[j] & r) | 0x20202020;
+    }
+
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, TEXTURE_SIZE, TEXTURE_SIZE, 
+                    GL_RGBA, GL_UNSIGNED_BYTE, mutated_data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    textures[i].handle = glGetTextureHandleARB(textures[i].name);
+    glMakeTextureHandleResidentARB(textures[i].handle);
+    pHandles[i * 2] = textures[i].handle;
   }
+
+  glUnmapBuffer(GL_UNIFORM_BUFFER);
+
+  load_shaders();
+
+  object.load("../../../media/objects/torus_nrms_tc.sbm");
+}
+
+void TextureLevels::render(double currentTime)
+{
+  static double last_time = 0.0;
+  static double total_time = 0.0;
+
+  const float f = (float)currentTime;
+
+  vmath::mat4 proj_matrix = vmath::perspective(70.0f,
+      (float)info.windowHeight / (float)info.windowHeight,
+      0.1f, 500.0f);
+
+  glViewport(0, 0, info.windowWidth, info.windowHeight);
+  glClearBufferfv(GL_COLOR, 0, sb7::color::Black);
+  glClearBufferfi(GL_DEPTH_STENCIL, 0, 1.0f, 0);
+
+  glFinish();
+
+  glBindBufferBase(GL_UNIFORM_BUFFER, 0, buffers.transformBuffer);
+  MATRICES* pMatrices = 
+      (MATRICES*)glMapBufferRange(GL_UNIFORM_BUFFER, 0, sizeof(MATRICES), 
+                                  GL_MAP_WRITE_BIT | 
+                                  GL_MAP_INVALIDATE_BUFFER_BIT);
+
+  pMatrices->view = vmath::translate(0.0f, 0.0f, -80.0f);
+  pMatrices->projection = proj_matrix;
+
+  float angle = f;
+  float angle2 = 0.7f * f;
+  float angle3 = 0.1f * f;
+  for (int i = 0; i < NUM_TEXTURES; ++i)
+  {
+    pMatrices->model[i] =
+        vmath::translate(float(i % 32)*4.0f - 62.0f,
+                         float(i >> 5) * 6.0f - 33.0f,
+                         15.0f * sinf(angle * 0.19f) + 3.0f *
+                         cosf(angle2 * 6.26f) + 30.0f * sinf(angle3))*
+        vmath::rotate(angle * 130.0f, 1.0f, 0.0f, 0.0f) *
+        vmath::rotate(angle * 140.0f, 0.0f, 0.0f, 1.0f);
+
+    angle += 1.0f;
+    angle2 += 4.1f;
+    angle3 += 0.01f;
+  }
+
+  glUnmapBuffer(GL_UNIFORM_BUFFER);
+
+  glFinish();
+
+  glBindBufferBase(GL_UNIFORM_BUFFER, 1, buffers.textureHandleBuffer);
+
+  glEnable(GL_DEPTH_TEST);
+
+  glUseProgram(program);
+
+  object.render(NUM_TEXTURES);
+}
+
+void TextureLevels::shutdown()
+{
+  for (int i = 0; i < NUM_TEXTURES; ++i)
+  {
+    glMakeTextureHandleNonResidentARB(textures[i].handle);
+    glDeleteTextures(1, &textures[i].name);
+  }
+
+  glDeleteProgram(program);
+}
+
+void TextureLevels::load_shaders()
+{
+  GLuint shaders[2];
+
+
 }
 
 DECLARE_MAIN(TextureLevels)
---
+++ OpenGLSamples/TunnelTexLev/TunnelTexLev/main.cpp
@@ -0,0 +1,175 @@
+#include <sb7.h>
+#include <sb7ktx.h>
+#include <vmath.h>
+
+#include <string>
+
+static void print_shader_log(GLuint shader)
+{
+  std::string str;
+  GLint len;
+
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+  if (len != 0)
+  {
+    str.resize(len);
+    glGetShaderInfoLog(shader, len, nullptr, &str[0]);
+  }
+
+#ifdef WIN32
+  OutputDebugStringA(str.c_str());
+#endif
+
+}
+
+#define GLSL(version, shader) "#version " #version "\n" #shader  
+
+static const GLchar* vertex_shader_source = GLSL
+(
+  450 core,
+  out VS_OUT
+  {
+    vec2 tc;
+  } vs_out;
+
+  uniform mat4 mvp;
+  uniform float offset;
+
+  void main(void)
+  {
+    const vec2[4] position = vec2[4](vec2(-0.5, -0.5),
+                                     vec2( 0.5, -0.5),
+                                     vec2(-0.5,  0.5),
+                                     vec2( 0.5,  0.5));
+
+    vs_out.tc = (position[gl_VertexID].xy + vec2(offset, 0.5)) * vec2(30.0, 1.0);
+
+    gl_Position = mvp * vec4(position[gl_VertexID], 0, 0, 1.0);
+  }
+);
+
+static const GLchar* fragment_shader_source = GLSL
+(
+  450 core,
+  layout (location = 0) out vec4 color;
+
+  in VS_OUT
+  {
+    vec2 tc;
+  } fs_in;
+
+  layout (binding = 0) uniform sampler2D tex;
+
+  void main(void)
+  {
+    color = texture(tex, fs_in.tc);
+  }
+);
+
+class Tunnel : public sb7::application
+{
+public:
+  virtual void startup() override
+  {
+    load_shaders();
+
+    uniforms.mvp = glGetUniformLocation(render_prog, "mvp");
+    uniforms.oofset = glGetUniformLocation(render_prog, "offset");
+
+    glGenVertexArrays(1, &render_vao);
+    glBindVertexArray(render_vao);
+
+    tex_wall = sb7::ktx::file::load("../../../media/textures/brick.ktx");
+    tex_ceiling = sb7::ktx::file::load("../../../media/textures/ceiling.ktx");
+    tex_floor = sb7::ktx::file::load("../../../media/textures/floor.ktx");
+
+    GLuint textures[] = { tex_floor, tex_wall, tex_ceiling };
+
+    for (int i = 0; i < 3; ++i)
+    {
+      glBindTexture(GL_TEXTURE_2D, textures[i]);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+
+    glBindVertexArray(render_vao);
+  }
+
+  virtual void render(double current_time) override
+  {
+    static const GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    float t = (float)current_time;
+
+    glViewport(0, 0, info.windowWidth, info.windowHeight);
+    glClearBufferfv(GL_COLOR, 0, black);
+
+    glUseProgram(render_prog);
+
+    vmath::mat4 proj_matrix = 
+        vmath::perspective(60.0f, (float)info.windowWidth / 
+                                  (float)info.windowHeight, 
+                           0.1f, 1000.0f);
+
+    glUniform1f(uniforms.oofset, t*0.03f);
+
+    GLuint textures[] = { tex_wall, tex_floor, tex_ceiling };
+
+    for (int i = 0; i < 3; ++i)
+    {
+      vmath::mat4 mv_matrix = vmath::rotate(90.0f*(float)i, 
+                                            vmath::vec3(0.0f, 0.0f, 1.0f)) *
+                              vmath::translate(-0.5f, 0.0f, -10.0f) *
+                              vmath::rotate(90.0f, 0.0f, 1.0f, 0.0f) *
+                              vmath::scale(30.0f, 1.0f, 1.0f);
+
+      vmath::mat4 mvp = proj_matrix * mv_matrix;
+
+      glUniformMatrix4fv(uniforms.mvp, 1, GL_FALSE, mvp);
+      glBindTexture(GL_TEXTURE_2D, textures[i]);
+      glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    }
+  }
+
+  void load_shaders()
+  {
+    char buffer[1024];
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+    glCompileShader(vertex_shader);
+
+    glGetShaderInfoLog(vertex_shader, 1024, nullptr, buffer);
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+    glCompileShader(fragment_shader);
+
+    glGetShaderInfoLog(fragment_shader, 1024, nullptr, buffer);
+
+    render_prog = glCreateProgram();
+    glAttachShader(render_prog, vertex_shader);
+    glAttachShader(render_prog, fragment_shader);
+
+    glLinkProgram(render_prog);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    glGetProgramInfoLog(render_prog, 1024, nullptr, buffer);
+  }
+protected:
+  GLuint render_prog;
+  GLuint render_vao;
+
+  struct
+  {
+    GLuint mvp;
+    GLuint oofset;
+  } uniforms;
+
+  GLuint tex_wall;
+  GLuint tex_ceiling;
+  GLuint tex_floor;
+};
+
+DECLARE_MAIN(Tunnel)
