0bb7c54e0512fd27113eea650c6849243c42d28e
DESKTOP-DR6OKQS\kathe <katherina.melnik96@gmail.com>
2018-05-06 10:07:28 +0300

    *.cpp code is finished in SpringMass project.

--- OpenGLSamples/SpringMass/SpringMass/main.cpp
+++ OpenGLSamples/SpringMass/SpringMass/main.cpp
@@ -37,7 +37,7 @@ protected:
   GLuint m_render_program;
 
   GLuint m_C_loc;
-  GLuint m_interation_index;
+  GLuint m_iteration_index;
 
   bool draw_points;
   bool draw_lines;
@@ -95,7 +95,7 @@ void SpringMass::startup()
   LoadShaders();
 
   vmath::vec4 *initial_positions = new vmath::vec4[POINTS_TOTAL];
-  vmath::vec4 *initial_velocities = new vmath::vec4[POINTS_TOTAL];
+  vmath::vec3 *initial_velocities = new vmath::vec3[POINTS_TOTAL];
   vmath::ivec4 *connection_vectors = new vmath::ivec4[POINTS_TOTAL];
 
   int n = 0;
@@ -112,7 +112,7 @@ void SpringMass::startup()
                                          (fj - 0.5f)*float(POINTS_Y),
                                          0.6f * sinf(fi) * cosf(fj),
                                          1.0f);
-      initial_velocities[n] = vmath::vec4(0.0f);
+      initial_velocities[n] = vmath::vec3(0.0f);
       connection_vectors[n] = vmath::ivec4(-1);
 
       if (j != (POINTS_Y - 1))
@@ -145,6 +145,129 @@ void SpringMass::startup()
     glBufferData(GL_ARRAY_BUFFER, POINTS_TOTAL * sizeof(vmath::vec4), 
                  initial_positions, GL_DYNAMIC_COPY);
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo[VELOCITY_A + i]);
+    glBufferData(GL_ARRAY_BUFFER, POINTS_TOTAL * sizeof(vmath::vec3), 
+                 initial_velocities, GL_DYNAMIC_COPY);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo[CONNECTION]);
+    glBufferData(GL_ARRAY_BUFFER, POINTS_TOTAL * sizeof(vmath::ivec4),
+                 connection_vectors, GL_STATIC_DRAW);
+    glVertexAttribPointer(2, 4, GL_INT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(2);
+  }
+
+  delete []connection_vectors;
+  delete []initial_velocities;
+  delete []initial_positions;
+
+  glGenTextures(2, m_pos_tbo);
+  glBindTexture(GL_TEXTURE_BUFFER, m_pos_tbo[0]);
+  glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, m_vbo[POSITION_A]);
+  glBindTexture(GL_TEXTURE_BUFFER, m_pos_tbo[1]);
+  glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, m_vbo[POSITION_B]);
+
+  int lines = (POINTS_X - 1) * POINTS_Y + (POINTS_Y - 1) * POINTS_X;
+
+  glGenBuffers(1, &m_index_buffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_index_buffer);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, lines * 2 * sizeof(int), 
+               nullptr, GL_STATIC_DRAW);
+
+  int *e = (int *)glMapBufferRange(GL_ELEMENT_ARRAY_BUFFER, 0, lines * 2 * sizeof(int), 
+                                   GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+
+  for (size_t j = 0; j < POINTS_Y; ++j)
+  {
+    for (size_t i = 0; i < POINTS_X - 1; ++i)
+    {
+      *e++ = i + j * POINTS_X;
+      *e++ = 1 + i + j * POINTS_X;
+    }
+  }
+
+  for (size_t i = 0; i < POINTS_X; ++i)
+  {
+    for (size_t j = 0; j < POINTS_Y - 1; ++j)
+    {
+      *e++ = i + j * POINTS_X;
+      *e++ = POINTS_X + i + j * POINTS_X;
+    }
+  }
+
+  glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+}
+
+void SpringMass::shutdown()
+{
+  glDeleteProgram(m_update_program);
+  glDeleteBuffers(5, m_vbo);
+  glDeleteVertexArrays(2, m_vao);
+}
+
+void SpringMass::render(double current_time)
+{
+  glUseProgram(m_update_program);
+
+  glEnable(GL_RASTERIZER_DISCARD);
+
+  for (size_t i = iterations_per_frame; i != 0; --i)
+  {
+    glBindVertexArray(m_vao[m_iteration_index & 1]);
+    glBindTexture(GL_TEXTURE_BUFFER, m_pos_tbo[m_iteration_index & 1]);
+    m_iteration_index++;
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, 
+                     m_vbo[POSITION_A + (m_iteration_index & 1)]);
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 1, 
+                     m_vbo[VELOCITY_A + (m_iteration_index & 1)]);
+    glBeginTransformFeedback(GL_POINTS);
+    glDrawArrays(GL_POINTS, 0, POINTS_TOTAL);
+    glEndTransformFeedback();
+  }
+
+  glDisable(GL_RASTERIZER_DISCARD);
+
+  static const GLfloat black[] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+  glViewport(0,0, info.windowWidth, info.windowHeight);
+  glClearBufferfv(GL_COLOR, 0, black);
+  
+  glUseProgram(m_render_program);
+
+  if (draw_points)
+  {
+    glPointSize(4.0f);
+    glDrawArrays(GL_POINTS, 0, POINTS_TOTAL);
+  }
+
+  if (draw_lines)
+  {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_index_buffer);
+    glDrawElements(GL_LINES, CONNECTIONS_TOTAL * 2, 
+                   GL_UNSIGNED_INT, nullptr);
+  }
+}
+
+void SpringMass::onKey(int key, int action)
+{
+  if (action)
+  {
+    switch (key)
+    {
+    case 'R': LoadShaders();
+      break;
+    case 'L': draw_lines = !draw_lines;
+      break;
+    case 'P': draw_points = !draw_points;
+      break;
+    case GLFW_KEY_KP_ADD: ++iterations_per_frame;
+      break;
+    case GLFW_KEY_KP_SUBTRACT: --iterations_per_frame;
+      break;
+    }
   }
 }
 
